ad3b27c41fa8998e7a266a0e88c5320acd7d64f9
Matin <mnasirii829@gmail.com>
2023-07-18 16:59:47 +0330

    Insert the Koin -di- Try to MVVM

--- app/src/main/java/com/example/firm/MyApp.kt
+++ app/src/main/java/com/example/firm/MyApp.kt
@@ -1,5 +1,34 @@
 package com.example.firm
 
-class MyApp {
+import android.app.Application
+import android.util.Log
+import android.widget.ImageView
+import com.example.firm.util.Const.TAG
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class MyApp : Application() {
     // Injection!
+    override fun onCreate() {
+        super.onCreate()
+        val myModules = module {
+            single<ImgLoader> { Pic() }
+        }
+
+        startKoin {
+            androidContext(this@MyApp)
+            modules(myModules)
+        }
+    }
+}
+
+interface ImgLoader {
+    fun loader(s: String, imgView: ImageView? = null)
+}
+
+class Pic : ImgLoader {
+    override fun loader(s: String, imgView: ImageView?) {
+        Log.v(TAG, s)
+    }
 }
